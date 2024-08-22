7b07344fc757ae0ba3613fa3684a19de708abfab
massygh <massyghen1@gmail.com>
2024-08-22 14:16:57 +0200

    add redme

--- NoSQL/12-log_stats.py
+++ NoSQL/12-log_stats.py
@@ -1,30 +1,31 @@
 #!/usr/bin/env python3
-"""Python script that provides stats about Nginx logs stored in MongoDB.
 """
+Log stats
+"""
+
 from pymongo import MongoClient
 
 
-def print_nginxlogs(nginx_collection):
-    """ Function prints stats about Nginx request logs.
-    """
-    print('{} logs'.format(nginx_collection.count_documents({})))
-    print('Methods:')
-    methods = ['GET', 'POST', 'PUT', 'PATCH', 'DELETE']
-    for method in methods:
-        req_count = len(list(nginx_collection.find({'method': method})))
-        print('\tmethod {}: {}'.format(method, req_count))
-    status_checks_count = len(list(
-        nginx_collection.find({'method': 'GET', 'path': '/status'})
-    ))
-    print('{} status check'.format(status_checks_count))
-
-
-def run():
-    """Function provides stats about Nginx logs stored in MongoDB.
-    """
+def log_stats():
+    """Provides some stats about Nginx logs stored in MongoDB"""
     client = MongoClient('mongodb://127.0.0.1:27017')
-    print_nginxlogs(client.logs.nginx)
+    collection = client.logs.nginx
+
+    # Number of logs
+    num_logs = collection.count_documents({})
+    print(f"{num_logs} logs")
+
+    # Methods
+    methods = ["GET", "POST", "PUT", "PATCH", "DELETE"]
+    print("Methods:")
+    for method in methods:
+        num_method = collection.count_documents({"method": method})
+        print(f"\tmethod {method}: {num_method}")
+
+    # Status check
+    num_status_check = collection.count_documents({"method": "GET", "path": "/status"})
+    print(f"{num_status_check} status check")
 
 
-if __name__ == '__main__':
-    run()
+if __name__ == "__main__":
+    log_stats()
