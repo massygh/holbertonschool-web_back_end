cf3ad5dc596f38b1b4983046b4e889c0444465f3
massygh <massyghen1@gmail.com>
2024-08-22 10:46:30 +0200

    add readme

---
+++ python_async_comprehension/0-async_generator.py
@@ -0,0 +1,7 @@
+import asyncio
+import random
+
+async def async_generator():
+    for _ in range(10):
+        await asyncio.sleep(1)  # Asynchronously wait for 1 second
+        yield random.uniform(0, 10)  # Yield a random number between 0 and 10
