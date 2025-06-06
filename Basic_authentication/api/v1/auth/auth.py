7b2fa6724124dd8182d4573bdb9231738fccc3cc
massygh <massyghen1@gmail.com>
2025-06-06 14:08:21 +0200

    test

--- Basic_authentication/api/v1/auth/auth.py
+++ Basic_authentication/api/v1/auth/auth.py
@@ -8,23 +8,23 @@ from typing import List, TypeVar
 class Auth:
     """ Template for all authentication system """
 
-def require_auth(self, path: str, excluded_paths: List[str]) -> bool:
-    """ Determines if authentication is required """
-    if path is None or excluded_paths is None or excluded_paths == []:
+    def require_auth(self, path: str, excluded_paths: List[str]) -> bool:
+        """ Determines if authentication is required """
+        if path is None or excluded_paths is None or excluded_paths == []:
+            return True
+
+        if not path.endswith('/'):
+            path += '/'
+
+        for excl_path in excluded_paths:
+            if excl_path.endswith('*'):
+                if path.startswith(excl_path[:-1]):
+                    return False
+            elif path == excl_path:
+                return False
+
         return True
 
-    # Assurer que path finit par un slash pour comparaison slash-tolerant
-    if not path.endswith('/'):
-        path += '/'
-
-    for excl_path in excluded_paths:
-        if excl_path.endswith('*'):
-            # Support futur pour wildcard (pas encore requis)
-            if path.startswith(excl_path[:-1]):
-                return False
-        elif path == excl_path:
-            return False
-        
     def authorization_header(self, request=None) -> str:
         """ Retrieves the authorization header from a request """
         return None
