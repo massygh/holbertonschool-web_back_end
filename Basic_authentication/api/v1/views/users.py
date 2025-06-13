8b722c94006a490d83db0a663c786b2d5e2f8565
massygh <massyghen1@gmail.com>
2025-06-13 12:31:13 +0200

    test

--- Basic_authentication/api/v1/views/users.py
+++ Basic_authentication/api/v1/views/users.py
@@ -10,8 +10,13 @@ from models.user import User
 def view_all_users() -> str:
     """ GET /api/v1/users
     Return:
-      - list of all User objects JSON represented
+      - list of all User objects JSON represented if authorized
+      - 403 if unauthorized
     """
+    user = auth.current_user(request)
+    if user is None:
+        abort(403)
+
     all_users = [user.to_json() for user in User.all()]
     return jsonify(all_users)
 
