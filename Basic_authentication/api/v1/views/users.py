2925a1364c49af3cd707cc2fd7978323e8a0e217
massygh <massyghen1@gmail.com>
2025-06-13 12:33:16 +0200

    test

--- Basic_authentication/api/v1/views/users.py
+++ Basic_authentication/api/v1/views/users.py
@@ -5,10 +5,6 @@ from api.v1.views import app_views
 from flask import abort, jsonify, request
 from models.user import User
 from api.v1.app import auth
-from api.v1.auth.auth import Auth
-from typing import Union, Optional
-from api.v1.auth.basic_auth import BasicAuth
-
 
 @app_views.route('/users', methods=['GET'], strict_slashes=False)
 def view_all_users() -> str:
