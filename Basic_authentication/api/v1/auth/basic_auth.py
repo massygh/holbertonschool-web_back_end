806c20eb8a441ea18ab3301e0ede10100a3be266
massygh <massyghen1@gmail.com>
2025-06-13 12:20:18 +0200

    test

--- Basic_authentication/api/v1/auth/basic_auth.py
+++ Basic_authentication/api/v1/auth/basic_auth.py
@@ -68,25 +68,23 @@ class BasicAuth(Auth):
         return user
 
     def current_user(self, request=None) -> TypeVar('User'):
-        try:
-            auth_header = self.authorization_header(request)
-            if auth_header is None:
-                return None
-
-            base64_auth = self.extract_base64_authorization_header(auth_header)
-            if base64_auth is None:
-                return None
-
-            decoded_auth = self.decode_base64_authorization_header(base64_auth)
-            if decoded_auth is None:
-                return None
-
-            email, password = self.extract_user_credentials(decoded_auth)
-            if email is None or password is None:
-                return None
-
-            return self.user_object_from_credentials(email, password)
-        except Exception as e:
-            print("Error in current_user:", e)
+        """
+        Retrieves the User instance for a request
+        """
+        auth_header = self.authorization_header(request)
+        if auth_header is None:
             return None
 
+        base64_auth = self.extract_base64_authorization_header(auth_header)
+        if base64_auth is None:
+            return None
+
+        decoded_auth = self.decode_base64_authorization_header(base64_auth)
+        if decoded_auth is None:
+            return None
+
+        email, password = self.extract_user_credentials(decoded_auth)
+        if email is None or password is None:
+            return None
+
+        return self.user_object_from_credentials(email, password)
