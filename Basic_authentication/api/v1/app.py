a461bd590f26c58cb19be64cd9ccf6e6c9aaa7e4
massygh <massyghen1@gmail.com>
2025-06-13 12:38:17 +0200

    test

--- Basic_authentication/api/v1/app.py
+++ Basic_authentication/api/v1/app.py
@@ -5,56 +5,37 @@ Route module for the API
 from os import getenv
 from api.v1.views import app_views
 from flask import Flask, jsonify, abort, request
-from flask_cors import CORS
-import os
+from flask_cors import (CORS, cross_origin)
 from api.v1.auth.auth import Auth
 from api.v1.auth.basic_auth import BasicAuth
+import os
+
 
 app = Flask(__name__)
 app.register_blueprint(app_views)
 CORS(app, resources={r"/api/v1/*": {"origins": "*"}})
 
+
+# Initialize auth
 auth = None
-AUTH_TYPE = os.getenv("AUTH_TYPE")
+auth_type = getenv("AUTH_TYPE")
 
-if AUTH_TYPE == 'auth':
-    from api.v1.auth.auth import Auth
-    auth = Auth()
-elif AUTH_TYPE == 'basic_auth':
-    from api.v1.auth.basic_auth import BasicAuth
+if auth_type == "basic_auth":
     auth = BasicAuth()
-
-
-@app.errorhandler(404)
-def not_found(error) -> str:
-    """ Not found handler """
-    return jsonify({"error": "Not found"}), 404
-
-
-@app.errorhandler(401)
-def unauthorized(error) -> str:
-    """ Unauthorized handler """
-    return jsonify({"error": "Unauthorized"}), 401
-
-
-@app.errorhandler(403)
-def forbidden(error) -> str:
-    """ Forbidden handler """
-    return jsonify({"error": "Forbidden"}), 403
+else:
+    auth = Auth()
 
 
 @app.before_request
-def before_request_func():
-    """ Method called before each request to check authentication """
+def before_request():
+    """ Before request handler
+    """
     if auth is None:
         return
-
-    excluded_paths = [
-        '/api/v1/status/',
-        '/api/v1/unauthorized/',
-        '/api/v1/forbidden/'
-    ]
-
+    # Public endpoints that do not require authentication
+    excluded_paths = ['/api/v1/status/', '/api/v1/unauthorized/',
+                      '/api/v1/forbidden/']
+    # Check if the request path requires authentication
     if not auth.require_auth(request.path, excluded_paths):
         return
 
@@ -63,6 +44,27 @@ def before_request_func():
 
     if auth.current_user(request) is None:
         abort(403)
+
+
+@app.errorhandler(404)
+def not_found(error) -> str:
+    """ Not found handler
+    """
+    return jsonify({"error": "Not found"}), 404
+
+
+@app.errorhandler(401)
+def unauthorized(error) -> str:
+    """ Unauthorized handler
+    """
+    return jsonify({"error": "Unauthorized"}), 401
+
+
+@app.errorhandler(403)
+def forbidden(error) -> str:
+    """ Forbidden handler
+    """
+    return jsonify({"error": "Forbidden"}), 403
 
 
 if __name__ == "__main__":
--- Basic_authentication/api/v1/views/users.py
+++ Basic_authentication/api/v1/views/users.py
@@ -4,19 +4,14 @@
 from api.v1.views import app_views
 from flask import abort, jsonify, request
 from models.user import User
-from api.v1.app import auth
+
 
 @app_views.route('/users', methods=['GET'], strict_slashes=False)
 def view_all_users() -> str:
     """ GET /api/v1/users
     Return:
-      - list of all User objects JSON represented if authorized
-      - 403 if unauthorized
+      - list of all User objects JSON represented
     """
-    user = auth.current_user(request)
-    if user is None:
-        abort(403)
-
     all_users = [user.to_json() for user in User.all()]
     return jsonify(all_users)
 
