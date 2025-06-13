660a2aea104dbba62075d37df6fddd5b684b0bef
massygh <massyghen1@gmail.com>
2025-06-13 12:01:42 +0200

    test

--- Basic_authentication/api/v1/auth/basic_auth.py
+++ Basic_authentication/api/v1/auth/basic_auth.py
@@ -26,3 +26,17 @@ class BasicAuth(Auth):
             return decoded_bytes.decode('utf-8')
         except Exception:
             return None
+
+    def extract_user_credentials(
+        self, decoded_base64_authorization_header: str
+    ) -> (str, str):
+        """
+        Extract user email and password from Base64 decoded value
+        """
+        if (decoded_base64_authorization_header is None or
+                not isinstance(decoded_base64_authorization_header, str) or
+                ':' not in decoded_base64_authorization_header):
+            return (None, None)
+
+        email, password = decoded_base64_authorization_header.split(':', 1)
+        return (email, password)
