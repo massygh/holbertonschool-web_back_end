e41fb48133eddc00bff3f9c595174f995fb51333
massygh <massyghen1@gmail.com>
2025-06-13 12:14:25 +0200

    test

--- Basic_authentication/api/v1/auth/basic_auth.py
+++ Basic_authentication/api/v1/auth/basic_auth.py
@@ -12,7 +12,6 @@ class BasicAuth(Auth):
     def decode_base64_authorization_header(
         self, base64_authorization_header: str
     ) -> str:
-
         """
         Decodes a Base64-encoded authorization header
         """
@@ -67,3 +66,25 @@ class BasicAuth(Auth):
             return None
 
         return user
+
+    def current_user(self, request=None) -> TypeVar('User'):
+        """
+        Retrieves the User instance for a request
+        """
+        auth_header = self.authorization_header(request)
+        if auth_header is None:
+            return None
+
+        base64_auth = self.extract_base64_authorization_header(auth_header)
+        if base64_auth is None:
+            return None
+
+        decoded_auth = self.decode_base64_authorization_header(base64_auth)
+        if decoded_auth is None:
+            return None
+
+        email, password = self.extract_user_credentials(decoded_auth)
+        if email is None or password is None:
+            return None
+
+        return self.user_object_from_credentials(email, password)
