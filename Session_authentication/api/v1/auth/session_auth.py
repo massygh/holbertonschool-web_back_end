5f3371778b61e3606c72eebc9e925a39642b6f7b
massygh <massyghen1@gmail.com>
2025-06-23 12:31:10 +0200

    test

--- Session_authentication/api/v1/auth/session_auth.py
+++ Session_authentication/api/v1/auth/session_auth.py
@@ -3,6 +3,7 @@
 SessionAuth module for session-based authentication.
 """
 from api.v1.auth.auth import Auth
+import uuid
 
 
 class SessionAuth(Auth):
@@ -10,4 +11,17 @@ class SessionAuth(Auth):
     SessionAuth class for handling session authentication. (Empty for now)
     Inherits from Auth.
     """
-    pass
+    # Attribut de classe pour stocker les sessions
+    user_id_by_session_id = {}
+
+    def create_session(self, user_id: str = None) -> str:
+        """
+        Crée un identifiant de session pour un user_id donné.
+        Retourne None si user_id est invalide.
+        Sinon, retourne le session_id (str) et l'enregistre dans le dict.
+        """
+        if user_id is None or not isinstance(user_id, str):
+            return None
+        session_id = str(uuid.uuid4())
+        self.user_id_by_session_id[session_id] = user_id
+        return session_id
