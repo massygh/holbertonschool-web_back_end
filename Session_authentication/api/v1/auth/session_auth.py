05e4f444dfd9bdba414bbb34a2bbd0a8afa036a7
massygh <massyghen1@gmail.com>
2025-06-23 12:39:03 +0200

    test

--- Session_authentication/api/v1/auth/session_auth.py
+++ Session_authentication/api/v1/auth/session_auth.py
@@ -4,6 +4,7 @@ SessionAuth module for session-based authentication.
 """
 from api.v1.auth.auth import Auth
 import uuid
+from models.user import User
 
 
 class SessionAuth(Auth):
@@ -34,3 +35,13 @@ class SessionAuth(Auth):
         if session_id is None or not isinstance(session_id, str):
             return None
         return self.user_id_by_session_id.get(session_id)
+
+    def current_user(self, request=None):
+        """
+        Retourne l'utilisateur courant basé sur le cookie de session dans la requête.
+        """
+        session_id = self.session_cookie(request)
+        user_id = self.user_id_for_session_id(session_id)
+        if user_id is None:
+            return None
+        return User.get(user_id)
