c1975ff888024a8c44ca726a81f304bbd876abc2
massygh <massyghen1@gmail.com>
2025-06-23 12:08:10 +0200

    first task

--- Session_authentication/api/v1/auth/session_auth.py
+++ Session_authentication/api/v1/auth/session_auth.py
@@ -4,6 +4,7 @@ SessionAuth module for session-based authentication.
 """
 from api.v1.auth.auth import Auth
 
+
 class SessionAuth(Auth):
     """
     SessionAuth class for handling session authentication. (Empty for now)
