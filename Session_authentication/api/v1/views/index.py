a8a4177a0af07b987cb604944796292838fd4e0e
massygh <massyghen1@gmail.com>
2025-06-23 11:20:58 +0200

    first task

---
+++ Session_authentication/api/v1/views/index.py
@@ -0,0 +1,43 @@
+#!/usr/bin/env python3
+""" Module of Index views
+"""
+from flask import jsonify, abort, Blueprint
+
+app_views = Blueprint('app_views', __name__, url_prefix='/api/v1')
+
+
+@app_views.route('/status', methods=['GET'], strict_slashes=False)
+def status() -> str:
+    """ GET /api/v1/status
+    Return:
+      - the status of the API
+    """
+    return jsonify({"status": "OK"})
+
+
+@app_views.route('/stats/', strict_slashes=False)
+def stats() -> str:
+    """ GET /api/v1/stats
+    Return:
+      - the number of each objects
+    """
+    from models.user import User
+    stats = {}
+    stats['users'] = User.count()
+    return jsonify(stats)
+
+
+@app_views.route('/unauthorized', methods=['GET'], strict_slashes=False)
+def unauthorized_route():
+    """ GET /api/v1/unauthorized
+        This route triggers a 401 Unauthorized error
+    """
+    abort(401)
+
+
+@app_views.route('/forbidden', methods=['GET'], strict_slashes=False)
+def forbidden_route():
+    """ GET /api/v1/forbidden
+        This route triggers a 403 Forbidden error
+    """
+    abort(403)
